442e579aa1e8420ce339a52a406c111838aaf2e9
hidang <yellowstudio.vn@gmail.com>
2020-06-01 20:35:48 +0700

    upxiusiu

--- Bai TH 4/Bai4/ctime.h
+++ Bai TH 4/Bai4/ctime.h
@@ -10,12 +10,23 @@ public:
     friend istream &operator>>(istream &is, ctime &ps);
     friend ostream &operator<<(ostream &os, ctime &ps);
     ctime operator+(int giays);
-    ctime operator-(const ctime &a);
+    ctime operator-(int);
 };
 
 ctime ctime::operator+(int giays)
 {
     if (giay + giays >= 60)
     {
+        giay = giay + giays - 60;
+        if (phut < 60)
+            phut++;
+        else if (gio < 24)
+            gio++;
+        else
+            gio = 0;
+    }
+    else
+    {
+        giay += giays;
     }
 }
